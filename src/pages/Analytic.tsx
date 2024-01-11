662cc17653451859641753fbf7a1fcebafc8416e
fauzandwip <fauzan27dp@gmail.com>
2024-01-11 11:21:48 +0700

    feat: add export exel funcionality

--- src/pages/Analytic.tsx
+++ src/pages/Analytic.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import CustomButton from '../components/CustomButton';
 import { DateRangePicker } from 'rsuite';
 import api from '../api';
 import { getToken } from '../helpers/localStorage';
+import { DownloadTableExcel } from 'react-export-table-to-excel';
 
 const AnalyticPage = () => {
 	// const [startDate, setStartDate] = useState('');
 	// const [endDate, setEndDate] = useState('');
+	const tableRef = useRef(null);
 	const [rangeDates, setRangeDates] = useState<string[]>([]);
 	const [analyticDatas, setAnalyticDatas] = useState<
 		{ scope: string; count: number }[][]
@@ -19,13 +21,28 @@ const AnalyticPage = () => {
 	console.log(scopes);
 
 	// const onSetDate = (startDate: string, endDate: string) => {};
+	const getTotalCount = (scope: string): number => {
+		let total = 0;
+		analyticDatas.forEach((data) => {
+			const count = data.find((el) => el.scope === scope)?.count;
+			if (count) total += count;
+			return total;
+		});
+		return total;
+	};
 
 	return (
 		<div className="w-full min-h-screen flex flex-col p-10 bg-cyan-500">
 			<div className="w-full p-4 flex justify-between items-center bg-cyan-300">
 				<h1 className="text-3xl font-bold">Analytic Reports</h1>
 				<div className="w-max">
-					<CustomButton value="Export Report" />
+					<DownloadTableExcel
+						filename="users table"
+						sheet="users"
+						currentTableRef={tableRef.current}
+					>
+						<CustomButton value="Export Report" />
+					</DownloadTableExcel>
 				</div>
 			</div>
 			<div className="w-full h-20 ps-4 flex items-center bg-cyan-200">
@@ -84,13 +101,18 @@ const AnalyticPage = () => {
 			</div>
 
 			<div className="relative overflow-x-auto">
-				<table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+				<table
+					ref={tableRef}
+					className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+				>
 					<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
 						<tr>
 							<th scope="col" className="px-6 py-3">
-								Product name
+								Item
 							</th>
-
+							<th scope="col" className="px-6 py-3">
+								Total
+							</th>
 							{Boolean(rangeDates.length) &&
 								rangeDates.map((data, index) => {
 									return (
@@ -115,6 +137,7 @@ const AnalyticPage = () => {
 										>
 											{scope}
 										</th>
+										<td>{getTotalCount(scope)}</td>
 										{Boolean(analyticDatas.length) &&
 											analyticDatas.map((data, index) => {
 												return (
